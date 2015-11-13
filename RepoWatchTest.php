8cea87c6dfd2fc4099198104b34565112987b730
Bueno, Feliphe <feliphezion@gmail.com>
2015-11-13 16:45:49 -0300

    Teste estúpido.
    Signed-off-by: Bueno, Feliphe <feliphezion@gmail.com>

---
+++ RepoWatchTest.php
@@ -0,0 +1,170 @@
+<?php
+
+$data = '
+{
+  "action": "created",
+  "comment": {
+    "url": "https://api.github.com/repos/feliphebueno/Sonne/comments/14166027",
+    "html_url": "https://github.com/feliphebueno/Sonne/commit/5e55de663a0f38b984730a642342112fcb281f48#commitcomment-14166027",
+    "id": 14166027,
+    "user": {
+      "login": "feliphebueno",
+      "id": 6662338,
+      "avatar_url": "https://avatars.githubusercontent.com/u/6662338?v=3",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/feliphebueno",
+      "html_url": "https://github.com/feliphebueno",
+      "followers_url": "https://api.github.com/users/feliphebueno/followers",
+      "following_url": "https://api.github.com/users/feliphebueno/following{/other_user}",
+      "gists_url": "https://api.github.com/users/feliphebueno/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/feliphebueno/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/feliphebueno/subscriptions",
+      "organizations_url": "https://api.github.com/users/feliphebueno/orgs",
+      "repos_url": "https://api.github.com/users/feliphebueno/repos",
+      "events_url": "https://api.github.com/users/feliphebueno/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/feliphebueno/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    "position": null,
+    "line": null,
+    "path": "",
+    "commit_id": "5e55de663a0f38b984730a642342112fcb281f48",
+    "created_at": "2015-11-03T20:31:04Z",
+    "updated_at": "2015-11-03T20:31:04Z",
+    "body": "LOOOOL"
+  },
+  "repository": {
+    "id": 44626298,
+    "name": "Sonne",
+    "full_name": "feliphebueno/Sonne",
+    "owner": {
+      "login": "feliphebueno",
+      "id": 6662338,
+      "avatar_url": "https://avatars.githubusercontent.com/u/6662338?v=3",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/feliphebueno",
+      "html_url": "https://github.com/feliphebueno",
+      "followers_url": "https://api.github.com/users/feliphebueno/followers",
+      "following_url": "https://api.github.com/users/feliphebueno/following{/other_user}",
+      "gists_url": "https://api.github.com/users/feliphebueno/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/feliphebueno/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/feliphebueno/subscriptions",
+      "organizations_url": "https://api.github.com/users/feliphebueno/orgs",
+      "repos_url": "https://api.github.com/users/feliphebueno/repos",
+      "events_url": "https://api.github.com/users/feliphebueno/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/feliphebueno/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    "private": false,
+    "html_url": "https://github.com/feliphebueno/Sonne",
+    "description": "Hier kommt die Sonne",
+    "fork": false,
+    "url": "https://api.github.com/repos/feliphebueno/Sonne",
+    "forks_url": "https://api.github.com/repos/feliphebueno/Sonne/forks",
+    "keys_url": "https://api.github.com/repos/feliphebueno/Sonne/keys{/key_id}",
+    "collaborators_url": "https://api.github.com/repos/feliphebueno/Sonne/collaborators{/collaborator}",
+    "teams_url": "https://api.github.com/repos/feliphebueno/Sonne/teams",
+    "hooks_url": "https://api.github.com/repos/feliphebueno/Sonne/hooks",
+    "issue_events_url": "https://api.github.com/repos/feliphebueno/Sonne/issues/events{/number}",
+    "events_url": "https://api.github.com/repos/feliphebueno/Sonne/events",
+    "assignees_url": "https://api.github.com/repos/feliphebueno/Sonne/assignees{/user}",
+    "branches_url": "https://api.github.com/repos/feliphebueno/Sonne/branches{/branch}",
+    "tags_url": "https://api.github.com/repos/feliphebueno/Sonne/tags",
+    "blobs_url": "https://api.github.com/repos/feliphebueno/Sonne/git/blobs{/sha}",
+    "git_tags_url": "https://api.github.com/repos/feliphebueno/Sonne/git/tags{/sha}",
+    "git_refs_url": "https://api.github.com/repos/feliphebueno/Sonne/git/refs{/sha}",
+    "trees_url": "https://api.github.com/repos/feliphebueno/Sonne/git/trees{/sha}",
+    "statuses_url": "https://api.github.com/repos/feliphebueno/Sonne/statuses/{sha}",
+    "languages_url": "https://api.github.com/repos/feliphebueno/Sonne/languages",
+    "stargazers_url": "https://api.github.com/repos/feliphebueno/Sonne/stargazers",
+    "contributors_url": "https://api.github.com/repos/feliphebueno/Sonne/contributors",
+    "subscribers_url": "https://api.github.com/repos/feliphebueno/Sonne/subscribers",
+    "subscription_url": "https://api.github.com/repos/feliphebueno/Sonne/subscription",
+    "commits_url": "https://api.github.com/repos/feliphebueno/Sonne/commits{/sha}",
+    "git_commits_url": "https://api.github.com/repos/feliphebueno/Sonne/git/commits{/sha}",
+    "comments_url": "https://api.github.com/repos/feliphebueno/Sonne/comments{/number}",
+    "issue_comment_url": "https://api.github.com/repos/feliphebueno/Sonne/issues/comments{/number}",
+    "contents_url": "https://api.github.com/repos/feliphebueno/Sonne/contents/{+path}",
+    "compare_url": "https://api.github.com/repos/feliphebueno/Sonne/compare/{base}...{head}",
+    "merges_url": "https://api.github.com/repos/feliphebueno/Sonne/merges",
+    "archive_url": "https://api.github.com/repos/feliphebueno/Sonne/{archive_format}{/ref}",
+    "downloads_url": "https://api.github.com/repos/feliphebueno/Sonne/downloads",
+    "issues_url": "https://api.github.com/repos/feliphebueno/Sonne/issues{/number}",
+    "pulls_url": "https://api.github.com/repos/feliphebueno/Sonne/pulls{/number}",
+    "milestones_url": "https://api.github.com/repos/feliphebueno/Sonne/milestones{/number}",
+    "notifications_url": "https://api.github.com/repos/feliphebueno/Sonne/notifications{?since,all,participating}",
+    "labels_url": "https://api.github.com/repos/feliphebueno/Sonne/labels{/name}",
+    "releases_url": "https://api.github.com/repos/feliphebueno/Sonne/releases{/id}",
+    "created_at": "2015-10-20T18:23:34Z",
+    "updated_at": "2015-10-22T21:29:11Z",
+    "pushed_at": "2015-10-22T21:29:11Z",
+    "git_url": "git://github.com/feliphebueno/Sonne.git",
+    "ssh_url": "git@github.com:feliphebueno/Sonne.git",
+    "clone_url": "https://github.com/feliphebueno/Sonne.git",
+    "svn_url": "https://github.com/feliphebueno/Sonne",
+    "homepage": null,
+    "size": 160,
+    "stargazers_count": 0,
+    "watchers_count": 0,
+    "language": "PHP",
+    "has_issues": true,
+    "has_downloads": true,
+    "has_wiki": true,
+    "has_pages": false,
+    "forks_count": 0,
+    "mirror_url": null,
+    "open_issues_count": 0,
+    "forks": 0,
+    "open_issues": 0,
+    "watchers": 0,
+    "default_branch": "master"
+  },
+  "sender": {
+    "login": "feliphebueno",
+    "id": 6662338,
+    "avatar_url": "https://avatars.githubusercontent.com/u/6662338?v=3",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/feliphebueno",
+    "html_url": "https://github.com/feliphebueno",
+    "followers_url": "https://api.github.com/users/feliphebueno/followers",
+    "following_url": "https://api.github.com/users/feliphebueno/following{/other_user}",
+    "gists_url": "https://api.github.com/users/feliphebueno/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/feliphebueno/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/feliphebueno/subscriptions",
+    "organizations_url": "https://api.github.com/users/feliphebueno/orgs",
+    "repos_url": "https://api.github.com/users/feliphebueno/repos",
+    "events_url": "https://api.github.com/users/feliphebueno/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/feliphebueno/received_events",
+    "type": "User",
+    "site_admin": false
+  }
+}';
+
+$data_string = \json_encode(\preg_replace('/[\s\r\t\n]/', '', $data));
+                                                                                                                     
+$ch = \curl_init('http://t1.virtuaserver.com.br/~siprevcloudcom/RepoWatch/');
+\curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, "POST");
+\curl_setopt($ch, \CURLOPT_POSTFIELDS, $data_string);
+\curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+\curl_setopt($ch, \CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . \strlen($data_string))
+);
+
+$response = \curl_exec($ch);
+$result = \json_decode($response, true);
+
+if(isset($result['sucesso'])){   
+    echo "Sucesso: ". $result['sucesso'];
+    if(\is_string($result['retorno'])){
+        print "\n\n<br><br> Retorno: ". $result['retorno'];
+    } else {
+        print "\n\n<br><br> Mesagem: ". $result['retorno']['mensagem'];
+        print "\n\n<br><br> Stack Trace: ". $result['retorno']['erro'];
+    }
+    
+} else {
+    print_r($response);
+}
